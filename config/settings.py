752139e49de2b14aabab9886fdaf42d56ca1b81e
aaafeig <geroy.ozhog08@mail.ru>
2026-02-10 15:35:59 +0300

    Выполнил 1 задание

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,10 @@ STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
--- users/views.py
+++ users/views.py
@@ -1,12 +1,26 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Payments
-from .serializers import PaymentsSerializer
+from .models import Payments, User
+from .serializers import PaymentsSerializer, UserSerializer
 
 
-
+#контроллер платежей
 class PaymentsListAPIView(generics.ListAPIView):
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
     filter_backends = (DjangoFilterBackend)
     filterset_fields = ('payment_date', 'course', 'lesson', 'payment_method')
+
+#контроллер пользователей
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
