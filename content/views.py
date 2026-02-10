803ce3432c38d02f0ddaff1514dd1636af09b56e
aaafeig <geroy.ozhog08@mail.ru>
2026-02-10 16:17:22 +0300

    Выполнил 3 задание

--- content/views.py
+++ content/views.py
@@ -1,18 +1,22 @@
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Lesson, Course
 from rest_framework import generics, viewsets
 from .serializers import LessonSerializer, CourseSerializer
-
+from .permission import IsOwnerOrModerator
 
 #Контроллеры для курсов(viewsets)
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    permission_classes = [IsOwnerOrModerator]
 
 #Контроллеры для уроков(generics)
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
@@ -21,10 +25,13 @@ class LessonListAPIView(generics.ListAPIView):
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrModerator]
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrModerator]
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsOwnerOrModerator]
